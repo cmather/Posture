ffea532b558291f0af0d34867fa5411ada8fd918
Chris Mather <mather.chris@gmail.com>
2012-06-15 15:41:37 -0700

    Removed TypeSpec and ModelSpec because I haven't implemented those yet

--- tests/spec/application/ModelSpec.js
+++
@@ -1,8 +0,0 @@
-define(["Posture/application/Model"], function(Posture) {
-  describe("Posture.application.Model", function() {
-    it ("should be defined", function() {
-      expect(Posture.application.Model).toBeDefined();
-    });
-    
-  });
-});

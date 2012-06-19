48f989bda8345a5aaeca4575ff0278ad6ce28298
Chris Mather <mather.chris@gmail.com>
2012-06-18 17:24:37 -0700

    Added Posture.elements

---
+++ tests/spec/elements/TableSpec.js
@@ -0,0 +1,7 @@
+define(["Posture/elements/Table"], function(Posture) {
+  describe("Posture.elements.Table", function() {
+    it ("should be defined", function() {
+      expect(Posture.elements.Table).toBeDefined();
+    });
+  });
+});

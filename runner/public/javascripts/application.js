b0b90a8117696c8329e8de203dbfeef3e4574ed6
Chris Mather <mather.chris@gmail.com>
2012-06-18 17:22:18 -0700

    This should just be a template for other developers to use to play around with the code.

--- runner/public/javascripts/application.js
+++ runner/public/javascripts/application.js
@@ -2,11 +2,7 @@ require.config({
   baseUrl: "javascripts/lib"
 });
 
-require(["Posture/Application"], function(Posture) {
-
-  window.Posture = Posture;
-
-  App = new Posture.Application({
-    bogus: new Posture.Namespace()
-  });
+require(["Posture/Component", "Posture/elements/Table"], function(Posture) {
+  /* You can play around here */
 });
+
